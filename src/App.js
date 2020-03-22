1e52b37e6bff1419d030d5d14f21a996401ff9bf
Paranthaman Rajendran <parantham@gmail.com>
2020-03-22 10:17:09 +0530

    setup finished for reactJS

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Development to test here !!!!
         </a>
       </header>
     </div>
